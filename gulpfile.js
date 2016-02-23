aa775ece4dd699d8747292f5885f768ba2e17473
Thien Nguyen <thien.nguyen@seldatinc.com>
2016-02-23 10:44:21 +0700

    Add Bootstrap & JQuery

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,8 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    var bootstrapPath = 'node_modules/bootstrap-sass/assets';
+    mix.sass('app.scss')
+        .browserify('app.js')
+        .copy(bootstrapPath + '/fonts', 'public/fonts');
 });
